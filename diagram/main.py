475a2bb13e33ac25765a71462bc4717e87b45715
Thien Phan <thien.phan@instacart.com>
2023-05-26 21:50:28 -0700

    updated changes

--- diagram/main.py
+++ diagram/main.py
@@ -6,10 +6,12 @@ from diagrams.onprem.ci import Jenkins, GithubActions, TravisCI
 from diagrams.aws.security import IAMPermissions
 from diagrams.aws.security import IdentityAndAccessManagementIam
 from diagrams.onprem.iac import Atlantis, Terraform
+from diagrams.aws.security import SecretsManager
+import json
 
 graph_attr = {
     "fontsize": "20",
-    "bgcolor": "transparent"
+    "bgcolor": "transparent",
     # "bgcolor": "white",
 }
 
@@ -30,10 +32,12 @@ with Diagram(
 
     with Cluster("AWS"):
         cloudtrail_logs = Cloudtrail("cloudtrail_logs")
-        cloudwatch_events = CloudwatchEventEventBased("cloudwatch_iam_events")
+        cloudwatch_events = CloudwatchEventEventBased("cloudwatch")
 
-        store_iam_policy_lambda = Lambda("store_iam_policy")
-        maintain_iam_policy_lambda = Lambda("maintain_iam_policy")
+        store_iam_policy_lambda = Lambda("store_lambda")
+        maintain_iam_policy_lambda = Lambda("maintain_lambda")
+
+        secret_manager = SecretsManager("secret_manager")
 
         resource_bucket = S3("policy_store")
         iam_permissions = IAMPermissions("iam_permissions")
@@ -57,3 +61,7 @@ with Diagram(
 
     # Lambda functions
     maintain_iam_policy_lambda >> iam_permissions
+
+    # Secrets manager
+    secret_manager >> maintain_iam_policy_lambda
+    secret_manager >> store_iam_policy_lambda
